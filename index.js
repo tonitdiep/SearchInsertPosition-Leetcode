cb59296480ac48273b032c4f08ab2f638abb218e
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-11 15:53:14 +0000

    soln 1

--- index.js
+++ index.js
@@ -7,6 +7,15 @@
 // Input: nums = [1,3,5,6], target = 5
 // Output: 2
 var searchInsert = function(nums, target) {
+    
+    let left = 0, right = nums.length - 1;
 
+      while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
+      }
+      return left;
 }
  
